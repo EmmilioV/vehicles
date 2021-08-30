36930acfa759041b740f556891c798304235a9f2
PABLO EMILIO VILLA PEREZ <pablovilla266477@correo.itm.edu.co>
2021-08-30 12:37:07 -0500

    Completed Procedures CRUD

---
+++ Vehicles.API/Migrations/20210830170156_AddTableProcedures.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vehicles.API.Migrations
+{
+    public partial class AddTableProcedures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_VehicleTypes",
+                table: "VehicleTypes");
+
+            migrationBuilder.RenameTable(
+                name: "VehicleTypes",
+                newName: "VehicleTypes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_VehicleTypes_Description",
+                table: "VehicleTypes",
+                newName: "IX_VehicleTypes_Description");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_VehicleTypes",
+                table: "VehicleTypes",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Procedures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Procedures", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Procedures_Description",
+                table: "Procedures",
+                column: "Description",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Procedures");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_VehicleTypes",
+                table: "VehicleTypes");
+
+            migrationBuilder.RenameTable(
+                name: "VehicleTypes",
+                newName: "VehiclesTypes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_VehicleTypes_Description",
+                table: "VehiclesTypes",
+                newName: "IX_VehiclesTypes_Description");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_VehiclesTypes",
+                table: "VehiclesTypes",
+                column: "Id");
+        }
+    }
+}
